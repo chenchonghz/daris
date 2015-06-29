d3080f0e6a4bd36bea1332cafc3220f9f00b33be
Wilson Liu <wliu5@unimelb.edu.au>
2015-06-29 16:51:17 +1000

    Work around ex-method (workflow) graph layout(display) issue after mfclientguigwt.jar upgrade.

--- daris-portal/src/main/java/daris/client/ui/exmethod/ExMethodDetails.java
+++ daris-portal/src/main/java/daris/client/ui/exmethod/ExMethodDetails.java
@@ -3,6 +3,7 @@ package daris.client.ui.exmethod;
 import arc.gui.form.Form;
 import arc.gui.form.FormEditMode;
 import arc.gui.gwt.colour.RGB;
+import arc.gui.gwt.widget.BaseWidget;
 import arc.gui.gwt.widget.button.Button;
 import arc.gui.gwt.widget.menu.MenuToolBar;
 import arc.gui.gwt.widget.panel.VerticalPanel;
@@ -29,143 +30,156 @@ import daris.client.ui.object.DObjectDetails;
 
 public class ExMethodDetails extends DObjectDetails {
 
-    public static final String TAB_NAME_WORKFLOW = "Experimental Workflow";
-    public static final String TAB_DESC_WORKFLOW = "Experimental Workflow";
+	public static final String TAB_NAME_WORKFLOW = "Experimental Workflow";
+	public static final String TAB_DESC_WORKFLOW = "Experimental Workflow";
 
-    public static ImageResource ZOOM_IN_ICON = Resource.INSTANCE.zoomIn16();
-    public static ImageResource ZOOM_OUT_ICON = Resource.INSTANCE.zoomOut16();
+	public static ImageResource ZOOM_IN_ICON = Resource.INSTANCE.zoomIn16();
+	public static ImageResource ZOOM_OUT_ICON = Resource.INSTANCE.zoomOut16();
 
+	public ExMethodDetails(DObjectRef po, ExMethod o, FormEditMode mode) {
 
-    public ExMethodDetails(DObjectRef po, ExMethod o, FormEditMode mode) {
+		super(po, o, mode);
 
-        super(po, o, mode);
+		updateWorkflowTab();
 
-        updateWorkflowTab();
-        
-        setActiveTab();
-    }
+		setActiveTab();
+	}
 
-    protected void addToInterfaceForm(Form interfaceForm) {
+	protected void addToInterfaceForm(Form interfaceForm) {
 
-        super.addToInterfaceForm(interfaceForm);
+		super.addToInterfaceForm(interfaceForm);
 
-        final ExMethod emo = (ExMethod) object();
-        XmlElement me = emo.methodElement();
-        if (me != null && interfaceForm.editMode() == FormEditMode.READ_ONLY) {
-            XmlMetaForm.addDocForView(interfaceForm, me);
-            // List<XmlElement> es = new Vector<XmlElement>();
-            // es.add(me);
-            // XmlMetaForm.addToForm(interfaceForm, es);
-        }
-    }
+		final ExMethod emo = (ExMethod) object();
+		XmlElement me = emo.methodElement();
+		if (me != null && interfaceForm.editMode() == FormEditMode.READ_ONLY) {
+			XmlMetaForm.addDocForView(interfaceForm, me);
+			// List<XmlElement> es = new Vector<XmlElement>();
+			// es.add(me);
+			// XmlMetaForm.addToForm(interfaceForm, es);
+		}
+	}
 
-    private void updateWorkflowTab() {
+	private void updateWorkflowTab() {
 
-        final ExMethod em = (ExMethod) object();
-        Graph graph = ExMethodGraph.graphFor(em);
-        final GraphWidget gw = new GraphWidget(graph);
+		final ExMethod em = (ExMethod) object();
+		Graph graph = ExMethodGraph.graphFor(em);
+		final GraphWidget gw = new GraphWidget(graph) {
+			private boolean _initLayout = true;
 
-        // TODO: temp fix for zoom in/out.
-        gw.setWidth(320);
-        gw.setHeight(240);
-        // gw.fitToParent();
+			protected void doLayoutChildren() {
+				if (_initLayout) {
+					parent().fitToParent();
+					_initLayout = false;
+				}
+				super.doLayoutChildren();
+			}
+		};
+		// gw.fitToParent();
 
-        gw.addGraphListener(new GraphListener() {
+		// TODO: temp fix for zoom in/out.
+		gw.setPreferredWidth(1024);
+		gw.setPreferredHeight(600);
+		// gw.setMinHeight(320);
+		// gw.setWidth(480);
+		// gw.setHeight(320);
+		// gw.fitToParent();
 
-            @Override
-            public void select(Node node) {
+		gw.addGraphListener(new GraphListener() {
 
-            }
+			@Override
+			public void select(Node node) {
 
-            @Override
-            public void deselect(Node node) {
+			}
 
-            }
+			@Override
+			public void deselect(Node node) {
 
-            @Override
-            public void open(Node node) {
+			}
 
-                if (node.isAtomic()) {
-                    final MethodAndStep mas = node.data();
-                    ExMethodStepRef emsr = new ExMethodStepRef(mas.rootMethod()
-                            .id(), mas.rootMethod().proute(), mas.stepPath());
-                    emsr.resolve(new ObjectResolveHandler<ExMethodStep>() {
+			@Override
+			public void open(Node node) {
 
-                        @Override
-                        public void resolved(ExMethodStep ems) {
+				if (node.isAtomic()) {
+					final MethodAndStep mas = node.data();
+					ExMethodStepRef emsr = new ExMethodStepRef(mas.rootMethod()
+							.id(), mas.rootMethod().proute(), mas.stepPath());
+					emsr.resolve(new ObjectResolveHandler<ExMethodStep>() {
 
-                            if (ems != null) {
-                                // StepEditorDialog dlg = new
-                                // StepEditorDialog(mas, ems);
-                                // dlg.show();
-                                // new StepEditAction(mas, ems,
-                                // window()).execute();
-                                new ExMethodStepDialog(mas, ems).show(window());
-                            }
-                        }
-                    });
-                }
-            }
-        });
+						@Override
+						public void resolved(ExMethodStep ems) {
 
-        VerticalPanel vp = new VerticalPanel();
-        vp.fitToParent();
+							if (ems != null) {
+								// StepEditorDialog dlg = new
+								// StepEditorDialog(mas, ems);
+								// dlg.show();
+								// new StepEditAction(mas, ems,
+								// window()).execute();
+								new ExMethodStepDialog(mas, ems).show(window());
+							}
+						}
+					});
+				}
+			}
+		});
 
-        MenuToolBar toolBar = new MenuToolBar();
-        toolBar.setHeight(25);
-        toolBar.setWidth100();
-        toolBar.setFontSize(11);
-        toolBar.setBackgroundColour(new RGB(0xf0, 0xf0, 0xf0));
-        toolBar.setPaddingTop(2);
-        toolBar.setPaddingBottom(2);
+		VerticalPanel vp = new VerticalPanel();
+		vp.fitToParent();
 
-        Button zoomInButton = new Button(
-                "<div style=\"cursor: pointer\"><img width=\""
-                        + ZOOM_IN_ICON.getWidth()
-                        + "px\" height=\""
-                        + ZOOM_IN_ICON.getHeight()
-                        + "px\" src=\""
-                        + ZOOM_IN_ICON.getSafeUri().asString()
-                        + "\" style=\"vertical-align: top; padding-top: 0px;\"></img>&nbsp;Zoom in</div>",
-                false);
-        zoomInButton.setWidth(90);
-        zoomInButton.addClickHandler(new ClickHandler() {
+		MenuToolBar toolBar = new MenuToolBar();
+		toolBar.setHeight(25);
+		toolBar.setWidth100();
+		toolBar.setFontSize(11);
+		toolBar.setBackgroundColour(new RGB(0xf0, 0xf0, 0xf0));
+		toolBar.setPaddingTop(2);
+		toolBar.setPaddingBottom(2);
 
-            @Override
-            public void onClick(ClickEvent event) {
+		Button zoomInButton = new Button(
+				"<div style=\"cursor: pointer\"><img width=\""
+						+ ZOOM_IN_ICON.getWidth()
+						+ "px\" height=\""
+						+ ZOOM_IN_ICON.getHeight()
+						+ "px\" src=\""
+						+ ZOOM_IN_ICON.getSafeUri().asString()
+						+ "\" style=\"vertical-align: top; padding-top: 0px;\"></img>&nbsp;Zoom in</div>",
+				false);
+		zoomInButton.setWidth(90);
+		zoomInButton.addClickHandler(new ClickHandler() {
 
-                gw.setWidth((int) (gw.width() * 1.1));
-                gw.setHeight((int) (gw.height() * 1.1));
-            }
-        });
-        toolBar.add(zoomInButton);
+			@Override
+			public void onClick(ClickEvent event) {
 
-        Button zoomOutButton = new Button(
-                "<div style=\"cursor: pointer\"><img width=\""
-                        + ZOOM_OUT_ICON.getWidth()
-                        + "px\" height=\""
-                        + ZOOM_OUT_ICON.getHeight()
-                        + "px\" src=\""
-                        + ZOOM_OUT_ICON.getSafeUri().asString()
-                        + "\" style=\"vertical-align: top; padding-top: 0px;\"></img>&nbsp;Zoom out</div>",
-                false);
-        zoomOutButton.setWidth(90);
-        zoomOutButton.addClickHandler(new ClickHandler() {
+				gw.setWidth((int) (gw.width() * 1.1));
+				gw.setHeight((int) (gw.height() * 1.1));
+			}
+		});
+		toolBar.add(zoomInButton);
 
-            @Override
-            public void onClick(ClickEvent event) {
+		Button zoomOutButton = new Button(
+				"<div style=\"cursor: pointer\"><img width=\""
+						+ ZOOM_OUT_ICON.getWidth()
+						+ "px\" height=\""
+						+ ZOOM_OUT_ICON.getHeight()
+						+ "px\" src=\""
+						+ ZOOM_OUT_ICON.getSafeUri().asString()
+						+ "\" style=\"vertical-align: top; padding-top: 0px;\"></img>&nbsp;Zoom out</div>",
+				false);
+		zoomOutButton.setWidth(90);
+		zoomOutButton.addClickHandler(new ClickHandler() {
 
-                gw.setWidth((int) (gw.width() / 1.1));
-                gw.setHeight((int) (gw.height() / 1.1));
-            }
-        });
-        toolBar.add(zoomOutButton);
+			@Override
+			public void onClick(ClickEvent event) {
 
-        vp.add(toolBar);
+				gw.setWidth((int) (gw.width() / 1.1));
+				gw.setHeight((int) (gw.height() / 1.1));
+			}
+		});
+		toolBar.add(zoomOutButton);
 
-        vp.add(new ScrollPanel(gw, ScrollPolicy.AUTO));
+		vp.add(toolBar);
 
-        setTab(TAB_NAME_WORKFLOW, TAB_DESC_WORKFLOW, vp);
-    }
+		vp.add(new ScrollPanel(gw, ScrollPolicy.AUTO));
+
+		setTab(TAB_NAME_WORKFLOW, TAB_DESC_WORKFLOW, vp);
+	}
 
 }
