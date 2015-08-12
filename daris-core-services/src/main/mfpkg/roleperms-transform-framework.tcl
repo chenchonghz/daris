c078ac344715a00e2f533d7b0625d2e53ea6ec0c
Wilson Liu <wliu5@unimelb.edu.au>
2015-08-12 17:17:44 +1000

    remove role namespace permissions for transform services.

--- daris-core-services/src/main/mfpkg/roleperms-transform-framework.tcl
+++ daris-core-services/src/main/mfpkg/roleperms-transform-framework.tcl
@@ -2,6 +2,4 @@
 if { [xvalue exists [package.exists :package "Transform Framework"]] == "true" || [xvalue exists [package.exists :package daris-transform]] == "true" } {
 	actor.grant :type role :name daris:pssd.model.user :role -type role transform.user
 	actor.grant :type role :name daris:pssd.object.admin :role -type role transform.admin
-    actor.grant :type plugin:service :name transform.create :perm < :resource -type role:namespace daris: :access ADMINISTER >
-    actor.grant :type plugin:service :name transform.execute :perm < :resource -type role:namespace daris: :access ADMINISTER >
 }
