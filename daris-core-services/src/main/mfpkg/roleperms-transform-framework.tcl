dd800a1344bc677e942d1fff9abec7e45f7f4b44
Wilson Liu <wliu5@unimelb.edu.au>
2015-08-12 17:06:55 +1000

    grant transform.create/execute services to access role namespace daris:

--- daris-core-services/src/main/mfpkg/roleperms-transform-framework.tcl
+++ daris-core-services/src/main/mfpkg/roleperms-transform-framework.tcl
@@ -1,6 +1,7 @@
-set PKG_TRANSFORM_FRAMEWORK "Transform Framework"
 
-if { [xvalue exists [package.exists :package ${PKG_TRANSFORM_FRAMEWORK}]] == "true" } {
+if { [xvalue exists [package.exists :package "Transform Framework"]] == "true" || [xvalue exists [package.exists :package daris-transform]] == "true" } {
 	actor.grant :type role :name daris:pssd.model.user :role -type role transform.user
 	actor.grant :type role :name daris:pssd.object.admin :role -type role transform.admin
+    actor.grant :type plugin:service :name transform.create :perm < :resource -type role:namespace daris: :access ADMINISTER >
+    actor.grant :type plugin:service :name transform.execute :perm < :resource -type role:namespace daris: :access ADMINISTER >
 }
