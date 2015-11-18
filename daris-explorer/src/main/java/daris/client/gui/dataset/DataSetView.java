3be8997cb4cfbefa018f5bbd3800cf9ae7f07599
Wilson Liu <wliu5@unimelb.edu.au>
2015-11-18 22:58:48 +1100

    added context menu for DICOM and NIFTI tab (title bar). to open the image in default web browser.

--- daris-explorer/src/main/java/daris/client/gui/dataset/DataSetView.java
+++ daris-explorer/src/main/java/daris/client/gui/dataset/DataSetView.java
@@ -1,9 +1,17 @@
 package daris.client.gui.dataset;
 
+import java.awt.Desktop;
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+import arc.mf.client.util.UnhandledException;
 import daris.client.gui.object.DObjectView;
 import daris.client.gui.xml.KVTreeTableView;
 import daris.client.model.dataset.DataSet;
 import daris.client.model.mime.MimeTypes;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.Tab;
 import javafx.scene.layout.StackPane;
 import javafx.scene.web.WebView;
@@ -23,7 +31,20 @@ public class DataSetView extends DObjectView<DataSet> {
             WebView web = new WebView();
             web.getEngine().load(dataset.dicomViewerUrl());
             dicomViewerPane.getChildren().add(web);
-            dicomViewerTab = new Tab("DCIOM", dicomViewerPane);
+            dicomViewerTab = new Tab("DICOM", dicomViewerPane);
+            ContextMenu menu = new ContextMenu();
+            MenuItem item = new MenuItem("Open it with default web browser...");
+            item.setOnAction(event -> {
+                try {
+                    java.awt.Desktop.getDesktop()
+                            .browse(new URI(dataset.dicomViewerUrl()));
+                } catch (Throwable e) {
+                    UnhandledException.report(
+                            "Opening DICOM Viewer in default web browser", e);
+                }
+            });
+            menu.getItems().add(item);
+            dicomViewerTab.setContextMenu(menu);
             getTabs().add(dicomViewerTab);
         }
         /*
@@ -34,9 +55,23 @@ public class DataSetView extends DObjectView<DataSet> {
             WebView web = new WebView();
             web.getEngine().load(dataset.niftiViewerUrl());
             niftiViewerPane.getChildren().add(web);
-            niftiViewerTab = new Tab("DCIOM", niftiViewerPane);
+            niftiViewerTab = new Tab("NIFTI", niftiViewerPane);
+            ContextMenu menu = new ContextMenu();
+            MenuItem item = new MenuItem("Open it with default web browser...");
+            item.setOnAction(event -> {
+                try {
+                    java.awt.Desktop.getDesktop()
+                            .browse(new URI(dataset.niftiViewerUrl()));
+                } catch (Throwable e) {
+                    UnhandledException.report(
+                            "Opening NIFTI Viewer in default web browser", e);
+                }
+            });
+            menu.getItems().add(item);
+            niftiViewerTab.setContextMenu(menu);
             getTabs().add(niftiViewerTab);
         }
+
     }
 
     @Override
