80c9f53eb7c967f699fa10b5294a1190880cebb9
Neil Killeen <nkilleen@unimelb.edu.au>
2016-03-10 17:04:35 +1100

    rensme 'processed' arg

--- daris-essentials/src/main/java/daris/essentials/SvcReplicateCheck.java
+++ daris-essentials/src/main/java/daris/essentials/SvcReplicateCheck.java
@@ -29,7 +29,7 @@ public class SvcReplicateCheck extends PluginService {
 		_defn.add(new Interface.Element("size",IntegerType.DEFAULT, "Limit the accumulation loop to this number of assets per iteration (if too large, the VM may run out of virtual memory).  Defaults to 500.", 0, 1));
 		_defn.add(new Interface.Element("dst", StringType.DEFAULT, "The destination parent namespace. If supplied (use '/' for root namespace), assets will actually be replicated (one at a time; not efficient). The default is no replication.", 0, 1));
 		_defn.add(new Interface.Element("mod", BooleanType.DEFAULT, "Check modification time of existing replicas (default false) as well as there existence.", 0, 1));
-		_defn.add(new Interface.Element("processed", BooleanType.DEFAULT, "By default, processed DataSets (ones for which (pssd-derivation/processed)='true' AND mf-dicom-series is absent) are excluded. Set to true to incluide.", 0, 1));
+		_defn.add(new Interface.Element("exclude-daris-proc", BooleanType.DEFAULT, "By default, processed DaRIS DataSets (ones for which (pssd-derivation/processed)='true' AND mf-dicom-series is absent) are included. Set to true to exclude these.", 0, 1));
 		_defn.add(new Interface.Element("use-indexes", BooleanType.DEFAULT, "Turn on or off the use of indexes in the query. Defaults to true.", 0, 1));
 		_defn.add(new Interface.Element("debug", BooleanType.DEFAULT, "Write some stuff in the log. Default to false.", 0, 1));
 	}
@@ -70,7 +70,7 @@ public class SvcReplicateCheck extends PluginService {
 		String size = args.stringValue("size", "500");
 		String dst = args.value("dst");
 		Boolean checkMod = args.booleanValue("mod", false);
-		Boolean processed = args.booleanValue("processed", false);
+		Boolean exclDaRISProc = args.booleanValue("exclude-daris-proc", false);
 		Boolean useIndexes = args.booleanValue("use-indexes", true);
 		Boolean dbg = args.booleanValue("debug", false);
 
@@ -90,7 +90,7 @@ public class SvcReplicateCheck extends PluginService {
 		boolean more = true;
 		Vector<String> assetIDs = new Vector<String>();
 		while (more) {
-			more = find (executor(),  where, peer, sr, uuidLocal, size, assetIDs, checkMod, processed, useIndexes,dbg,  w);
+			more = find (executor(),  where, peer, sr, uuidLocal, size, assetIDs, checkMod, exclDaRISProc, useIndexes,dbg,  w);
 			if (dbg) {
 				System.out.println("nig.replicate.check : checking for abort \n");
 			}
@@ -127,14 +127,14 @@ public class SvcReplicateCheck extends PluginService {
 	}
 
 	private boolean find (ServiceExecutor executor,  String where, String peer, ServerRoute sr, String uuidLocal, String size, 
-			Vector<String> assetList, Boolean checkMod, Boolean processed, Boolean useIndexes, Boolean dbg, XmlWriter w)
+			Vector<String> assetList, Boolean checkMod, Boolean exclDaRISProc, Boolean useIndexes, Boolean dbg, XmlWriter w)
 					throws Throwable {
 
 		// Find local  assets  with the given query. We work through the cursor else
 		// we may run out of memory
 		System.out.println("nig.replicate.check : find assets on primary in chunk starting with idx = " + idx_);
 		XmlDocMaker dm = new XmlDocMaker("args");
-		if (!processed) {
+		if (exclDaRISProc) {
 			// Drop processed DataSets from query
 			if (where==null) where = "";
 			// Need this complex expression as there the simpler generates a NULLP
@@ -233,47 +233,6 @@ public class SvcReplicateCheck extends PluginService {
 					}
 				}
 			}
-
-			/*
-		for (XmlDoc.Element asset : assets) {
-			// Get the asset id, and the rid (asset may already be a replica from elsewhere)
-			String id = asset.value("@id");
-			String cid = asset.value("cid");
-			String csize = asset.value("content/size");
-
-			// If the asset is already a replica, its rid remains the same
-			// when replicated to another peer
-			String rid = asset.value("rid");    
-
-			// If primary, set expected rid on remote peer
-			if (rid==null) rid = uuidLocal + "." + id;
-
-			// Find it 
-			dm = new XmlDocMaker("args");
-			dm.add("rid", rid);
-			XmlDoc.Element r2 = executor.execute(sr, "asset.exists", dm.root());
-			if (r2.booleanValue("exists")==false) {
-				w.add("id", new String[]{"exists", "false", "cid", cid, "size", csize},  id);
-				assetList.add(id);
-			} else {
-				// The asset exists as a replica, but perhaps it's been modified.
-				// Very time consuming...
-				if (checkMod) {
-					// See if the primary has been modified since the replica was made
-					Date mtime = asset.dateValue("mtime");
-
-					dm = new XmlDocMaker("args");
-					dm.add("id", r2.value("exists/@id"));
-					XmlDoc.Element remoteAsset = executor.execute(sr, "asset.get", dm.root());
-					Date mtimeRep = remoteAsset.dateValue("asset/mtime");
-					// System.out.println("mtimes=" + mtime + ", " + mtimeRep);
-					if (mtime.after(mtimeRep)) {
-						w.add("id", new String[]{"exists", "true", "cid", cid, "size", csize},  id);
-						assetList.add(id);	
-					}
-				}
-			}
-			*/
 		}
 		//
 		return more;
