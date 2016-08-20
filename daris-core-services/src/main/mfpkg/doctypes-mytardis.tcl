053c6fe0013c1e9f08569a3673fad269393f5a87
Wilson Liu <wliu5@unimelb.edu.au>
2016-08-20 23:26:20 +1000

    added doc types for mytardis experiment and dataset.

--- daris-core-services/src/main/mfpkg/doctypes-mytardis.tcl
+++ daris-core-services/src/main/mfpkg/doctypes-mytardis.tcl
@@ -1,9 +1,9 @@
-asset.doc.type.update :create true :type daris:mytardis-experiment :index true :definition < \
+asset.doc.type.update :create true :type daris:mytardis-experiment :index all :definition < \
     :element -name id -type long -min-occurs 1 -max-occurs 1 \
     :element -name title -type string -min-occurs 0 -max-occurs 1 \
     :element -name description -type string -min-occurs 0 -max-occurs 1 >
 
-asset.doc.type.update :create true :type daris:mytardis-dataset :index true :definition < \
+asset.doc.type.update :create true :type daris:mytardis-dataset :index all :definition < \
     :element -name id -type long -min-occurs 1 -max-occurs 1 \
     :element -name description -type string -min-occurs 0 -max-occurs 1 \
     :element -name instrument -type string -min-occurs 0 -max-occurs 1 \
