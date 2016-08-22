8676e26a4e1e82d27d2348a8b9646009ab9b3986
Wilson Liu <wliu5@unimelb.edu.au>
2016-08-22 12:44:14 +1000

    added uri element to mytardis-experiment

--- daris-core-services/src/main/mfpkg/doctypes-mytardis.tcl
+++ daris-core-services/src/main/mfpkg/doctypes-mytardis.tcl
@@ -1,7 +1,8 @@
 asset.doc.type.update :create true :type daris:mytardis-experiment :index all :definition < \
     :element -name id -type long -min-occurs 1 -max-occurs 1 \
     :element -name title -type string -min-occurs 0 -max-occurs 1 \
-    :element -name description -type string -min-occurs 0 -max-occurs 1 >
+    :element -name description -type string -min-occurs 0 -max-occurs 1 \
+    :element -name uri -type url -min-occurs 1 -max-occurs 1 >
 
 asset.doc.type.update :create true :type daris:mytardis-dataset :index all :definition < \
     :element -name id -type long -min-occurs 1 -max-occurs 1 \
