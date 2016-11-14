57a485152e186936c631aef9a469ead315b34bc4
Wilson Liu <wliu5@unimelb.edu.au>
2016-11-14 13:06:33 +1100

    module lib namespace changed..

--- daris-core-services/src/main/mfpkg/plugin-module-add.tcl
+++ daris-core-services/src/main/mfpkg/plugin-module-add.tcl
@@ -1,9 +1,9 @@
 set plugin_label      [string toupper PACKAGE_$package]
-set plugin_namespace  mflux/plugins
+set plugin_namespace  mflux/plugins/daris-core-services
 set plugin_zip        daris-core-services-plugin.zip
 set plugin_jar        daris-core-services-plugin.jar
 set module_class      nig.mf.plugin.pssd.PSSDPluginModule
-set plugin_libs       { libs/daris-commons.jar libs/daris-dcmtools.jar }
+set plugin_libs       { lib/daris-commons-1.0.4.jar lib/daris-dcmtools-1.0.1.jar }
 
 # import the plugin jar from the zip file to Mediaflux system. It will be an asset
 # in the specified namespace with plugin jar file as content.
@@ -12,14 +12,23 @@ asset.import :url archive:${plugin_zip} \
 	:label -create yes ${plugin_label} :label PUBLISHED \
     :update true
 
-# add plugin module
-if { [xvalue exists [plugin.module.exists :path ${plugin_namespace}/${plugin_jar} :class ${module_class}]] == "false" } {
-    set svc_args " :path ${plugin_namespace}/${plugin_jar} :class ${module_class}"
-    foreach lib ${plugin_libs} {
-        set svc_args "${svc_args} :lib $lib"
-    }
-	plugin.module.add ${svc_args}
+# clean up old version of the module
+if { [xvalue exists [plugin.module.exists :path /mflux/plugins/daris-core-services-plugin.jar :class nig.mf.plugin.pssd.PSSDPluginModule]] == "true" } {
+    plugin.module.remove :path /mflux/plugins/daris-core-services-plugin.jar :class nig.mf.plugin.pssd.PSSDPluginModule
+    asset.hard.destroy :id "path=/mflux/plugins/daris-core-services-plugin.jar"
 }
+
+# remove module
+if { [xvalue exists [plugin.module.exists :path ${plugin_namespace}/${plugin_jar} :class ${module_class}]] == "true" } {
+    plugin.module.remove :path ${plugin_namespace}/${plugin_jar} :class ${module_class}
+}
+
+# add module
+set command "plugin.module.add :path ${plugin_namespace}/${plugin_jar} :class ${module_class}"
+foreach lib ${plugin_libs} {
+    set command "${command} :lib $lib"
+}
+eval ${command}
     
 # Now that the plugins have been registered, we need to refresh the known services
 # with this session so that we can grant permissions for those plugins.
--- daris-core-services/src/main/mfpkg/plugin-module-remove.tcl
+++ daris-core-services/src/main/mfpkg/plugin-module-remove.tcl
@@ -1,9 +1,15 @@
 set plugin_label      [string toupper PACKAGE_$package]
-set plugin_namespace  mflux/plugins
+set plugin_namespace  mflux/plugins/daris-core-services
 set plugin_zip        daris-core-services-plugin.zip
 set plugin_jar        daris-core-services-plugin.jar
 set module_class      nig.mf.plugin.pssd.PSSDPluginModule
-set plugin_libs       { libs/daris-commons.jar libs/daris-dcmtools.jar }
+set plugin_libs       { lib/daris-commons-1.0.4.jar lib/daris-dcmtools-1.0.1.jar }
+
+# clean up old version of the module
+if { [xvalue exists [plugin.module.exists :path /mflux/plugins/daris-core-services-plugin.jar :class nig.mf.plugin.pssd.PSSDPluginModule]] == "true" } {
+    plugin.module.remove :path /mflux/plugins/daris-core-services-plugin.jar :class nig.mf.plugin.pssd.PSSDPluginModule
+    asset.hard.destroy :id "path=/mflux/plugins/daris-core-services-plugin.jar"
+}
 
 # remove the plugin module
 if { [xvalue exists [plugin.module.exists :path ${plugin_namespace}/${plugin_jar} :class ${module_class}]] == "true" } {
